227f1c6778a43e0b41bbc124eb17509e7be76f7a
00shadow00 <48408201+00shadow00@users.noreply.github.com>
2026-08-16 10:09:57 +0800

    Update index.ts

--- ai/index.ts
+++ ai/index.ts
@@ -4,7 +4,7 @@ import { experimental_wrapLanguageModel as wrapLanguageModel } from "ai";
 import { customMiddleware } from "./custom-middleware";
 
 export const geminiProModel = wrapLanguageModel({
-  model: google("gemini-2.5-flash"),
+  model: google("gemini-3.5-flash-lite"),
   middleware: customMiddleware,
 });
 
